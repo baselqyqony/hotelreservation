3b9409e391680b8acd52669dab797ab95e486b06
ahmad <ahmad.ah.reda@gmail.com>
2017-10-19 09:57:03 +0200

    Add Room class...

--- core/src/main/java/hu/uni/miskolc/iit/sweng/hotelReservation/model/room.java
+++ core/src/main/java/hu/uni/miskolc/iit/sweng/hotelReservation/model/room.java
@@ -2,8 +2,37 @@ package hu.uni.miskolc.iit.sweng.hotelReservation.model;
 
 public class room {
     private int number;
-    private int RoomType type;
+    private  RoomType type;
     private int capacity;
 
+    public room(int number, RoomType type, int capacity) {
+        this.number = number;
+        this.type = type;
+        this.capacity = capacity;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+    public void setType(RoomType type) {
+        this.type = type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
 
