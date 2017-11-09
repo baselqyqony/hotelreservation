7eb96594e9482c3747b75e548e0cb1e742627595
ahmad <ahmad.ah.reda@gmail.com>
2017-11-09 12:09:11 +0100

    Add exceptions package into the Service modify Service interfaces to throw exceptions Add JavaDoc description to Room Class

--- core/src/main/java/hu/uni/miskolc/iit/sweng/hotelReservation/model/Room.java
+++ core/src/main/java/hu/uni/miskolc/iit/sweng/hotelReservation/model/Room.java
@@ -1,22 +1,47 @@
 package hu.uni.miskolc.iit.sweng.hotelReservation.model;
 
 /**
- * Created by Ahmad Reda on 2017.10.19..
+ * hold room data
+ * @author Ahmad Reda
+ *@version 1.0
+ * @since 09 - 11 - 2017
+ *
  */
 public class Room {
+    /**
+     * room number
+     */
     private int number;
+    /**
+     * room type
+     */
     private  RoomType type;
+    /**
+     * room capacity(number of beds);
+     */
     private int capacity;
 
+    /**
+     * Room class constructor
+     * @param number room number
+     * @param type room type
+     * @param capacity capacity of room
+     */
     public Room(int number, RoomType type, int capacity) {
         this.number = number;
         this.type = type;
         this.capacity = capacity;
     }
 
+    /**
+     *
+     *
+     * @return int  This returns room number
+     */
     public int getNumber() {
         return number;
     }
+
 
     public void setNumber(int number) {
         this.number = number;
